285a6102cd9dbddf72f6d01274e9c5cb736a59eb
williambrito98 <williambrito199850@gmail.com>
2022-05-19 01:25:11 -0300

    fix: create browser

--- src/components/Browser/index.ts
+++ src/components/Browser/index.ts
@@ -1,10 +1,10 @@
-import { launch, Browser, connect, Page } from 'puppeteer-core'
+import puppeteer from 'puppeteer-core'
 
 export default class HandleBrowser {
   // @ts-ignore
-  public browser : Browser
+  public browser : puppeteer.Browser
   // @ts-ignore
-  public page : Page
+  public page : puppeteer.Page
 
   public async create () {
     if (process.env.TYPE_CONNECTION_BROWSER === 'ws') {
@@ -16,19 +16,20 @@ export default class HandleBrowser {
     return { browser: this.browser, page: this.page }
   }
 
-  private createBrowserWSEndpont () : Promise<Browser> {
-    return connect({})
+  private createBrowserWSEndpont () : Promise<puppeteer.Browser> {
+    return puppeteer.connect({})
   }
 
-  private async createDefaultBrowser () : Promise<Browser> {
-    return launch({
+  private async createDefaultBrowser () : Promise<puppeteer.Browser> {
+    return puppeteer.launch({
       executablePath: process.env.EXECUTABLE_PATH,
       args: process.env.ARGS?.split(','),
       headless: process.env.HEADLESS !== 'false',
       slowMo: Number(process.env.SLOW_MO),
-      ignoreHTTPSErrors: process.env.IGNORE_HTTTP_ERRORS !== 'false',
+      ignoreHTTPSErrors: process.env.IGNORE_HTTP_ERRORS !== 'false',
       userDataDir: process.env.USER_DATA_DIR,
-      defaultViewport: JSON.parse(process.env.DEFAULT_VIEW_PORT || '')
+      defaultViewport: JSON.parse(process.env.DEFAULT_VIEW_PORT || ''),
+      ignoreDefaultArgs: process.env.IGNORE_DEFAULT_ARGS?.split(',')
     })
   }
 }
