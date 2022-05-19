c51130ec8e3ca5609e81a96a94f07d2a61617851
williambrito98 <williambrito199850@gmail.com>
2022-05-19 00:27:49 -0300

    chore: edit eslint config

---
+++ src/App.ts
@@ -0,0 +1,5 @@
+export default class App {
+  public main() {
+    console.log('main');
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import App from './App';
+
+(async () => {
+  const app = new App()
+  app.main()
+})()
