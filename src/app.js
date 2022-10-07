d53c31e47923180c36aa5c964f6192e734a350bd
williambrito98 <williambrito199850@gmail.com>
2022-10-07 12:57:28 -0300

    refactor: create puppteerAdapter and try again on error

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 const makeBrowser = require('./factories/makeBrowser')
-const { join } = require('path')
-const resolveCaptcha = require('./utils/captcha/resolve')
 const { WrongUserKey, ZeroBalance } = require('./errors/captcha')
-const { rmSync } = require('fs')
-
+const makePage = require('./factories/makePage')
 /**
  *
  * @param {{values : Array, __root_dir : string}} data
@@ -11,26 +8,50 @@ const { rmSync } = require('fs')
  * @param {Function(string)} log
  */
 module.exports = async (data, selectors, log) => {
-  const { browser, page } = await makeBrowser()
   try {
-    await page.goto(selectors.site_url, { waitUntil: 'networkidle0' })
-    const imageCaptcha = await page.$('#imgCaptcha')
-    await imageCaptcha.screenshot({
-      path: join(global.pathTemp, 'captcha.png'),
-      type: 'png'
-    })
-    const captchaResolved = await resolveCaptcha(join(global.pathTemp, 'captcha.png'))
-    await browser.close()
+    let browser, page, index
+    ({ browser } = await makeBrowser())
+    try {
+      ({ page } = await makePage(browser))
+      await page.goto(selectors.site_url, { waitUntil: 'networkidle0' })
+      console.log('template robots')
+      await browser.close()
+      return {
+        status: true
+      }
+    } catch (error) {
+      console.log(error)
+      if (error instanceof WrongUserKey) {
+        log('Erro ao enviar o usuario para o serviço de captcha')
+        return {
+          status: true,
+          continue: false,
+          error: error.message
+        }
+      }
+      if (error instanceof ZeroBalance) {
+        log('Erro ao descriptografar captcha: não a saldo disponivel')
+        return {
+          status: true,
+          continue: false,
+          error: error.message
+        }
+      }
+      log(error)
+      return {
+        status: false,
+        continue: true,
+        repeat: true,
+        lastIndex: index,
+        error: error?.message
+      }
+    }
   } catch (error) {
-    if (error instanceof WrongUserKey) {
-      log('Erro ao enviar o usuario para o serviço de captcha')
+    log('Erro ao inicializar robo')
+    return {
+      status: false,
+      continue: false,
+      error: error?.message
     }
-    if (error instanceof ZeroBalance) {
-      log('Erro ao descriptografar captcha: não a saldo disponivel')
-    }
-    console.log(error)
-  } finally {
-    rmSync(global.pathTemp, { force: true, recursive: true })
-    process.exit()
   }
 }
--- src/factories/makePage.js
+++ src/factories/makePage.js
@@ -1,11 +1,11 @@
 const PuppeteerAdapter = require('../utils/puppeteer/PuppeteerAdapter')
 const { getEnvBrowser } = require('../utils/env')
 
-module.exports = async function () {
+module.exports = async function (browser) {
   const config = getEnvBrowser()
   const puppeteerAdapter = new PuppeteerAdapter()
-  const browser = await puppeteerAdapter.handleBrowser(config)
+  const page = await puppeteerAdapter.handlePage(browser, config)
   return {
-    browser
+    page
   }
 }
