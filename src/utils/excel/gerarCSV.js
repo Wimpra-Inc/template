187d50c229a054c1d2bc62de3a8c71154d07f914
williambrito98 <williambrito199850@gmail.com>
2025-01-30 23:02:19 -0300

    fix: create report

--- src/utils/excel/gerarCSV.js
+++ src/utils/excel/gerarCSV.js
@@ -1,13 +1,14 @@
 const { join } = require("path")
 const connection = require("../../database/connection")
 const { appendFileSync } = require('fs')
+const { isJson } = require("../validators")
 
 module.exports = async () => {
     const conn = await connection()
 
     let dados = await conn.table('processing').where({ processed: true }).select('dados').distinct()
 
-    dados = dados.flatMap(dado => dado.dados ? JSON.parse(dado.dados) : []).map(dado => Object.values(dado).join(';')).join('\n')
+    dados = dados.flatMap(dado => dado.dados && isJson(dado.dados) ? JSON.parse(dado.dados) : dados.dados ? [dado.dados] : []).map(dado => Object.values(dado).join(';')).join('\n')
 
     appendFileSync(join(global.PATH_SAIDA, 'dados.csv'), dados)
 
