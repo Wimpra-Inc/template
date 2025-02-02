9ae4b1a80f6e7b5dd772bef1ea35231b4afb6477
williambrito98 <williambrito199850@gmail.com>
2025-02-02 20:56:44 -0300

    fix: cpfcnpj validator

--- src/utils/validators/cpfCnpj.js
+++ src/utils/validators/cpfCnpj.js
@@ -1,6 +1,6 @@
-const { cnpj, cpf } = require('cpf-cnpj-validator')
+const { cnpj, cpf } = require("cpf-cnpj-validator");
 
 module.exports = function (value) {
-  if (cpf.isValid(value || cnpj.isValid(value))) return true
-  return false
-}
+    if (cpf.isValid(value) || cnpj.isValid(value)) return true;
+    return false;
+};
