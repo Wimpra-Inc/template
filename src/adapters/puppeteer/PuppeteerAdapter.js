57b5b9c50b4fb8da02d9f5d8c5aa95b384e3fba0
williambrito98 <williambrito199850@gmail.com>
2025-02-07 18:04:14 -0300

    fix: command chrome start

--- src/adapters/puppeteer/PuppeteerAdapter.js
+++ src/adapters/puppeteer/PuppeteerAdapter.js
@@ -1,68 +1,73 @@
-const puppeteer = require('puppeteer')
-const { readdirSync } = require('fs')
-const { setTimeout } = require('timers/promises')
-const DownloadTimeoutError = require('../../errors/browser/DownloadTimeoutError')
-const { exec } = require('child_process')
-const get = require('../../utils/request/get')
-const BrowserConnectionError = require('../../errors/browser/BrowserConnectionError')
-const { env } = require('../../env')
-const { URL } = require('url')
+const puppeteer = require("puppeteer");
+const { readdirSync } = require("fs");
+const { setTimeout } = require("timers/promises");
+const DownloadTimeoutError = require("../../errors/browser/DownloadTimeoutError");
+const { exec } = require("child_process");
+const get = require("../../utils/request/get");
+const BrowserConnectionError = require("../../errors/browser/BrowserConnectionError");
+const { env } = require("../../env");
+const { URL } = require("url");
 
 class PuppeteerAdapter {
-
-    #page = null
+    #page = null;
     /**
-      * @param config {import('puppeteer-core').LaunchOptions & import('puppeteer-core').BrowserConnectOptions & import('puppeteer-core').BrowserLaunchArgumentOptions}
-      * @returns {import('puppeteer-core').Browser & {closeAllPages: Function}}
-      */
+     * @param config {import('puppeteer-core').LaunchOptions & import('puppeteer-core').BrowserConnectOptions & import('puppeteer-core').BrowserLaunchArgumentOptions}
+     * @returns {import('puppeteer-core').Browser & {closeAllPages: Function}}
+     */
     async handleBrowser(config) {
-        if (global.browser && global.browser.isConnected()) return global.browser
+        if (global.browser && global.browser.isConnected())
+            return global.browser;
 
         if (env.CREATE_BROWSER_BY_WS_ENDPOINT) {
-            const chromeCommandStart = this.#getChromeCommandStart()
-            exec(chromeCommandStart)
-            await setTimeout(5000)
-            const chromeDebugData = await get(env.CHROME_REMOTE_DEBUGGING_URL)
-            const webSocketDebuggerUrl = chromeDebugData?.data?.webSocketDebuggerUrl
+            const chromeCommandStart = this.#getChromeCommandStart();
+            exec(chromeCommandStart);
+            await setTimeout(5000);
+            const chromeDebugData = await get(env.CHROME_REMOTE_DEBUGGING_URL);
+            const webSocketDebuggerUrl =
+                chromeDebugData?.data?.webSocketDebuggerUrl;
             if (!webSocketDebuggerUrl) {
-                throw new BrowserConnectionError('Não foi possível obter o webSocketDebuggerUrl')
+                throw new BrowserConnectionError(
+                    "Não foi possível obter o webSocketDebuggerUrl"
+                );
             }
             global.browser = await puppeteer.connect({
                 browserWSEndpoint: webSocketDebuggerUrl,
-                ...config
-            })
+                ...config,
+            });
 
-            global.browser.closeAllPages = () => this.closeAllPages(global.browser)
+            global.browser.closeAllPages = () =>
+                this.closeAllPages(global.browser);
 
-            return global.browser
+            return global.browser;
         }
-        global.browser = await puppeteer.launch(config)
-        global.browser.closeAllPages = () => this.closeAllPages(global.browser)
+        global.browser = await puppeteer.launch(config);
+        global.browser.closeAllPages = () => this.closeAllPages(global.browser);
 
-        return global.browser
+        return global.browser;
     }
 
     /**
-      * @param config {import('puppeteer-core').LaunchOptions & import('puppeteer-core').BrowserConnectOptions & import('puppeteer-core').BrowserLaunchArgumentOptions}
-      * @param browser {import('puppeteer-core').Browser}
-      * @returns {import('puppeteer-core').Page & {setDownloadDirectory: Function, waitForDownload: Function, clearAllCookies: Function, closeAllPages: Function}}
-      */
+     * @param config {import('puppeteer-core').LaunchOptions & import('puppeteer-core').BrowserConnectOptions & import('puppeteer-core').BrowserLaunchArgumentOptions}
+     * @param browser {import('puppeteer-core').Browser}
+     * @returns {import('puppeteer-core').Page & {setDownloadDirectory: Function, waitForDownload: Function, clearAllCookies: Function, closeAllPages: Function}}
+     */
     async handlePage(browser, config) {
-        this.#page = await browser.newPage()
+        this.#page = await browser.newPage();
 
-        this.#page.setDefaultTimeout(config.defaultTimeout)
-        this.#page.setDefaultNavigationTimeout(config.defaultNavigationTimeout)
+        this.#page.setDefaultTimeout(config.defaultTimeout);
+        this.#page.setDefaultNavigationTimeout(config.defaultNavigationTimeout);
 
-        await this.#page.setViewport(config.defaultViewport)
+        await this.#page.setViewport(config.defaultViewport);
 
-        config.pathDownload ? await this.setDownloadDirectory(config.pathDownload) : ''
+        config.pathDownload
+            ? await this.setDownloadDirectory(config.pathDownload)
+            : "";
 
-        this.#page.setDownloadDirectory = this.setDownloadDirectory
-        this.#page.waitForDownload = this.waitForDownload
-        this.#page.clearAllCookies = () => this.clearAllCookies()
+        this.#page.setDownloadDirectory = this.setDownloadDirectory;
+        this.#page.waitForDownload = this.waitForDownload;
+        this.#page.clearAllCookies = () => this.clearAllCookies();
 
-
-        return this.#page
+        return this.#page;
     }
 
     /**
@@ -71,53 +76,55 @@ class PuppeteerAdapter {
      * @param page  {import('puppeteer-core').Page}
      */
     async setDownloadDirectory(path) {
-        const client = await this.#page.target().createCDPSession()
-        await client.send('Page.setDownloadBehavior', {
+        const client = await this.#page.target().createCDPSession();
+        await client.send("Page.setDownloadBehavior", {
             downloadPath: path,
-            behavior: 'allow'
-        })
+            behavior: "allow",
+        });
     }
-
 
     /**
      *   @param browser {import('puppeteer-core').Browser}
-    */
+     */
     async closeAllPages(browser) {
         try {
-            const pages = await browser.pages()
+            const pages = await browser.pages();
             for (const page of pages) {
-                await page.close()
+                await page.close();
             }
-        } catch (error) { }
+        } catch (error) {}
     }
 
     async clearAllCookies() {
-        const client = await this.#page.target().createCDPSession()
-        await client.send('Network.clearBrowserCookies')
+        const client = await this.#page.target().createCDPSession();
+        await client.send("Network.clearBrowserCookies");
     }
 
     async waitForDownload(pathDownload, limit = 0) {
         if (limit >= 60000) {
-            throw new DownloadTimeoutError('Limite para o download excedido')
+            throw new DownloadTimeoutError("Limite para o download excedido");
         }
-        const string = readdirSync(pathDownload).join('')
-        if (!string.includes('crdownload')) {
-            if (env.FILE_NAME_DOWNLOAD && !string.includes(env.FILE_NAME_DOWNLOAD)) {
-                await setTimeout(1500)
-                return await this.waitForDownload(pathDownload, limit + 1500)
+        const string = readdirSync(pathDownload).join("");
+        if (!string.includes("crdownload")) {
+            if (
+                env.FILE_NAME_DOWNLOAD &&
+                !string.includes(env.FILE_NAME_DOWNLOAD)
+            ) {
+                await setTimeout(1500);
+                return await this.waitForDownload(pathDownload, limit + 1500);
             }
         }
 
-        return true
+        return true;
     }
 
     #getChromeCommandStart() {
-        const url = new URL(env.CHROME_REMOTE_DEBUGGING_URL)
-        if (process.platform === 'win32') {
-            return `"${env.EXECUTABLE_PATH} --remote-debugging-port=${url.port}"`
+        const url = new URL(env.CHROME_REMOTE_DEBUGGING_URL);
+        if (process.platform === "win32") {
+            return `"${env.EXECUTABLE_PATH}" --remote-debugging-port=${url.port}`;
         }
-        return `${env.EXECUTABLE_PATH} --remote-debugging-port=${url.port}`
+        return `${env.EXECUTABLE_PATH} --remote-debugging-port=${url.port}`;
     }
 }
 
-module.exports = PuppeteerAdapter
+module.exports = PuppeteerAdapter;
