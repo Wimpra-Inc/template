153a0305b3d4ec235620192db44876f9442c3341
williambrito1998 <williambrito199850@gmail.com>
2025-07-20 20:36:07 -0300

    fix: typing return connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,34 +1,31 @@
 const { join } = require('path')
 const knex = require('knex')
 
-global.connection = null;
+global.connection = null
 
-
-async function isConnected() {
-    try {
-        await global.connection.raw('select 1')
-        return true
-    } catch (error) {
-        return false
-    }
-
+async function isConnected () {
+  try {
+    await global.connection.raw('select 1')
+    return true
+  } catch (error) {
+    return false
+  }
 }
 
 /**
- * @returns {import('knex').Knex}
+ * @returns {Promise<import('knex').Knex>}
  */
 module.exports = async () => {
-    if (await isConnected()) {
-        return global.connection
-    }
-    global.connection = knex({
-        client: 'sqlite3',
-        connection: {
-            filename: join(global.PATH_PROCESSANDO, 'database.sqlite')
-        },
-        useNullAsDefault: true
-    })
-
-
+  if (await isConnected()) {
     return global.connection
+  }
+  global.connection = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: join(global.PATH_PROCESSANDO, 'database.sqlite')
+    },
+    useNullAsDefault: true
+  })
+
+  return global.connection
 }
