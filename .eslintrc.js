36c53f541b835107bbff0db4787c3905b016cb65
williambrito1998 <williambrito199850@gmail.com>
2025-07-20 20:34:14 -0300

    chore: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      es2021: true,
-      node: true
-    },
-    extends: 'standard',
-    overrides: [
-      {
-        env: {
-          node: true
-        },
-        files: [
-          '.eslintrc.{js,cjs}'
-        ],
-        parserOptions: {
-          sourceType: 'script'
-        }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
       }
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest'
-    },
-    rules: {
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prefer-const': 'warn',
+    'no-unused-expressions': 'off'
+  }
 }
